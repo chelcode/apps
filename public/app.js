10898b278578fefe0277328de91ce1e703f609b2
chechen <chelseachen2014@gmail.com>
2016-01-21 00:31:10 -0800

    angular js file

--- public/app.js
+++ public/app.js
@@ -4,12 +4,21 @@ var app = angular.module('eventConf', []);
     $scope.sessions = [];
     $http.get('http://localhost:9899/eventConf')
         .success(function(data){
+            angular.forEach(data, function(ele, index){
+               //Just add the index to your item
+               ele.index = index;
+            });
+            jQuery("#message").html("");
             $scope.sessions = data;
             console.log($scope.sessions);
         })
         .error(function(data){
             console.log('Error: ' + data);        
         });
+
+    $scope.downloadCSV = function(){
+        window.open('csv', '_blank');
+    }
   }])
   .directive('session', function() {
     return {
