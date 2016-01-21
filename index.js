09409132ad882b0bde30ab66c0f41fc489861a55
chechen <chelseachen2014@gmail.com>
2016-01-21 00:32:10 -0800

    node server file

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	assign = require('object-assign'),
 	https = require('https'),
+	json2csv = require('json2csv'),
 	fs = require('fs');
 
 //------------------------------------------------------------------------------
@@ -20,7 +21,6 @@ var express = require('express'),
 //------------------------------------------------------------------------------
 
 var data = {},
-	id = 0,
 	app = express();
 
 //------------------------------------------------------------------------------
@@ -43,21 +43,29 @@ app.get('/eventConf', function(req, res) {
 	https.get('https://registration.gputechconf.com/cubehenge/json.php/GTC.MobileGuestServices.getSessions/GTC%202015/true', function(resData) {
 		
 		console.log('statusCode: ', resData.statusCode);
-  		console.log('headers: ', resData.headers);
+  console.log('headers: ', resData.headers);
 
-        var body = ""; // a container for the response
+  var body = ""; // a container for the response
 		resData.on('data', function(d) {
 		    body += d;
 		});
 
 		resData.on('end', function() {
-      		//res.send(obj);
+      		
            var obj = JSON.parse(body),
            	   sessionIds, sessionId, aSpeakerInfo,
                aSpeaker, speakers = {}, i, j;
-           
+           var sessionsClone = [];
+												
+
            //Task1 - Speaker Info
            for(i=0; i<obj.length; i++) {
+             var clone = {}
+             Object.getOwnPropertyNames(obj[i]).forEach(function (name) {
+													  if(name !== "speakerInfo")
+												      clone[name] = obj[i][name];
+												 });
+             sessionsClone.push(clone);
 
            		sessionId = obj[i].id;
            		aSpeakerInfo = obj[i].speakerInfo;
@@ -75,7 +83,8 @@ app.get('/eventConf', function(req, res) {
            				speakers[aSpeaker.name] = aSpeaker;
            			}
            		}
-           	}
+            }
+
            	console.log("Task 1 (Speaker Information):");
            	for(prop in speakers) {
            		aSpeaker = speakers[prop];
@@ -105,7 +114,7 @@ app.get('/eventConf', function(req, res) {
               speakerArr=[];
 
             for(prop in organzations) {
-              //console.log("speakers." + prop );
+              
               speakerArr = organzations[prop]['sperakers'];
               
               sessionArr=[];
@@ -128,6 +137,22 @@ app.get('/eventConf', function(req, res) {
                 	organzations[prop]['sperakers'].length + " employee(s) that spoke at " 
                 	+ organzations[prop]['sessions'].length + " session(s). ");
             }
+
+            //Task 4 - write to csv file
+            var fields = Object.keys(sessionsClone[0]);
+ 
+												json2csv({ data: sessionsClone, fields: fields }, function(err, csv) {
+												  if (err) console.log(err);
+												  //console.log(csv);
+               fs.writeFile('public/event.csv', csv, 'utf8', function (err) {
+																  if (err) {
+																    console.log('Some error occured - file either not saved or corrupted file saved.');
+																  } else{
+																    console.log('It\'s saved!');
+																  }
+															});
+												});
+
             res.send(obj); 
            });
 
@@ -146,6 +171,12 @@ app.get('/', function(req, res) {
 	
 	res.sendfile('./public/index.html'); // load the single view file 
 });
+
+app.get('/csv', function(req, res) {
+	
+	res.sendfile('./public/event.csv'); // load the single view file 
+});
+
 
 app.listen(9899);
 console.log('Started listening on port 9899');
